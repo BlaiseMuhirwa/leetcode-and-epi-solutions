33913855d1bc4be3199465ecbdd7b6d33d3f1d0a
Blaise Muhirwa <blaisemuhirwa3@gmail.com>
2020-07-28 19:53:56 -0400

    Modify Dynamic_Programming/ClimbingStairs.java

--- Dynamic_Programming/ClimbingStairs.java
+++ Dynamic_Programming/ClimbingStairs.java
@@ -1,5 +1,29 @@
 package Dynamic_Programming;
 
 public class ClimbingStairs {
-    
+    /* 
+    This algorithm runs in O(n) time with O(n) space
+    */
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else if (n == 2) {
+            return 2;
+        }
+        
+        int[] array = new int[n];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        for (int i = 3; i<n; i++) {
+            array[i] = array[i-1] + array[i-2];
+        }
+        return array[n-1]; 
+    }
+
 }
+
