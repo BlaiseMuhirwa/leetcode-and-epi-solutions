ca7c529a7d8593422c55f4d95c8654bb1eaab620
Blaise Muhirwa <blaisemuhirwa3@gmail.com>
2020-07-29 14:56:20 -0400

    Add SingleNumber and ContainsDuplicate

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -1,5 +1,18 @@
 package Arrays;
 
 public class ContainsDuplicate {
-    
+    /* Runtime: O(n); space complexity: O(n) */
+    public boolean containsDuplicate(int[] nums) {
+        if(nums == null || nums.length == 1) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.putIfAbsent(nums[i], 1) != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
---
+++ Arrays/SingleNumber.java
@@ -0,0 +1,29 @@
+package Arrays;
+import java.util.*;
+public class SingleNumber {
+    /* the algorithm runs in O(nlogn) time with O(1) extra space 
+        Notice that this algorithm can also run in O(n) with O(n) extra space */
+    public int singleNumber(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        int value = 0;
+        for (int i = 1; i < nums.length-1; i++) {
+            /* check if the unique number if at the start */
+            if (nums[i] != nums[0] && i == 1) {
+                value = nums[0];
+            }
+            /* check if the unique number is in the middle */
+            else if (nums[i] != nums[i+1] && nums[i] != nums[i-1]) {
+                value = nums[i];
+            }
+            /* check if the unique number is at the end */
+            else if (i+1 == nums.length-1 && nums[i+1] != nums[i]) {
+                value = nums[i+1];
+            }
+           
+        }
+        return value;
+    }
+}
