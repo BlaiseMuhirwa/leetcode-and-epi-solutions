baf2ea4cc410f2632d4f881828ff01ba421ab16a
Blaise Muhirwa <blaisemuhirwa3@gmail.com>
2020-08-05 03:12:14 -0400

    Add the is subsequence problem

---
+++ Dynamic_Programming/IsSubsequence.java
@@ -0,0 +1,32 @@
+package Dynamic_Programming;
+
+/*
+Given a string s and a string t, check if s is subsequence of t.
+A subsequence of a string is a new string which is formed from 
+the original string by deleting some (can be none) of the characters 
+without disturbing the relative positions of the remaining characters. 
+(ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Follow up:
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B,
+ and you want to check one by one to see if T has its subsequence.
+  In this scenario, how would you change your code?
+
+*/
+public class IsSubsequence {
+    /* This algorithm runs in O(n) time with O(1) space */
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() > t.length()) return false;
+        if (s.equals("") && t != null) return true;
+        
+        int index = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) == s.charAt(index)) {
+                index++;
+            }
+            if (index >= s.length()) return true;
+        }
+        return false;
+        
+    }
+}
