b497bedcdeb813400d42431eb73b590ba2fb05cc
Blaise Muhirwa <blaisemuhirwa3@gmail.com>
2020-08-09 03:37:17 -0400

    Add combination sum 2 problem

---
+++ Backtracking/CombinationsSums2.java
@@ -0,0 +1,47 @@
+package backtracking;
+import java.util.*;
+/*
+Given a collection of candidate numbers (candidates) and a target number (target),
+ find all unique combinations in candidates where the candidate numbers sums to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+*/
+public class CombinationsSums2 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> combinations = new ArrayList<List<Integer>>();
+        Arrays.sort(candidates);
+        findCombinations(combinations, candidates,target, 0, new ArrayList<>());
+        return combinations;
+    }
+    
+    private void findCombinations(List<List<Integer>> combinations, int[] candidates,
+                                  int target, int start, ArrayList<Integer> current) {
+        if (target == 0) {
+            if (!combinations.contains(current)) {
+                combinations.add(new ArrayList<>(current));
+            }
+            return;
+        }
+        else if (target < 0) return;
+
+        for (int i = start; i < candidates.length; i++) {
+            current.add(candidates[i]);
+            findCombinations(combinations, candidates, target - candidates[i], i+1, current);
+            current.remove(current.size() - 1);
+        }
+        
+    }
+}
