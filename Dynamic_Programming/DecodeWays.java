27bef50eadd38fa17dad734015f08c96aa38efd4
Blaise Munyampirwa <blaisemuhirwa3@gmail.com>
2020-08-30 02:07:12 -0500

    add bottom-up solution for decode ways

--- Dynamic_Programming/DecodeWays.java
+++ Dynamic_Programming/DecodeWays.java
@@ -41,4 +41,25 @@ public class DecodeWays {
         memo.put(index, ans);
         return ans;
     }
+    /* bottom-up dynamic programming approach */
+    public int numDecodingsDynamicProgramming(String s) {
+        if (s == null) return 0;
+        if (s.length() == 0) return 0;
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        
+        for (int i = 2; i <= s.length(); i++) {
+            int oneDigit = Integer.parseInt(s.substring(i-1, i));
+            int twoDigits = Integer.parseInt(s.substring(i-2, i));
+            if (oneDigit >= 1) {
+                dp[i] += dp[i-1];
+            }
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[s.length()];
+    }
+    }
 }
