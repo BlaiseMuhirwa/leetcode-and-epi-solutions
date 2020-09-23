a1dacd3790ac83f49d0055a781cefb5a7b9abe5d
Blaise Munyampirwa <blaisemuhirwa3@gmail.com>
2020-09-22 22:40:29 -0500

    add the intersection of two linked lists problem

---
+++ Linked_List/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,28 @@
+package Linked_List;
+
+/**
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pa = headA;
+        ListNode pb = headB;
+
+        while (pa != pb) {
+            pa = (pa != null) ? pa.next : headB;
+            pb = (pb != null) ? pb.next : headA;
+        }
+        return pa;
+    }
+}
